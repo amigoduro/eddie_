74e78e963383713b2772ee549ff3df36f1986848
Cadet Adarsh <pal.adarsh10205@gmail.com>
2026-02-13 02:30:23 +0530

    Add eligibility check for blood donation

---
+++ one.py
@@ -0,0 +1,8 @@
+age = int(input("Enter age: "))
+weight = float(input("Enter weight (kg): "))
+
+if age >= 18 and weight >= 50:
+    print("Eligible for blood donation")
+else:
+    print("Not eligible for blood donation")
+
