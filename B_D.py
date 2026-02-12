50877740fbdc9c59702995bce57cf32738c0bb41
Cadet Adarsh <pal.adarsh10205@gmail.com>
2026-02-12 11:14:27 +0530

    Add criteria check for blood donation eligibility

---
+++ B_D.py
@@ -0,0 +1,16 @@
+print("Criteria Check For B.Donation")
+age = int(input("Enter Your Age"))
+if age>=18:
+    print("Step 1 Check...")
+else:
+        print("Sorry better luck luck next year")
+    
+
+
+weight = int(input("Enter Your Weigth"))
+if weight>=50:
+    print("Step 2 Check...")
+    print("You can donate the blood")
+else:
+        print("You are out of weight")
+    
