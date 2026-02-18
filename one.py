f4e1362a42f53530054cab350b97448e777cdcfd
Cadet Adarsh <pal.adarsh10205@gmail.com>
2026-02-18 23:13:17 +0530

    Update one.py

--- one.py
+++ one.py
@@ -4,5 +4,5 @@ weight = float(input("Enter weight (kg): "))
 if age >= 18 and weight >= 50:
     print("Eligible for blood donation")
 else:
-    print("Not eligible for blood donation")
+    print("Not eligible for blood donation")   
 
