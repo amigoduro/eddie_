f51d5a8c66f4a588934cf643e8149607bfc33b71
Cadet Adarsh <pal.adarsh10205@gmail.com>
2026-04-01 22:38:39 +0530

    Clean up whitespace and formatting in contradic.py
    Fix formatting and remove unnecessary whitespace.

--- contradic.py
+++ contradic.py
@@ -4,14 +4,12 @@ x = 0
 
 print("x\t sin(x)\t cos(x)\t tan(x)")
 
-
 while x <= 10:
     
     sin_x = math.sin(x)
     cos_x = math.cos(x)
     tan_x = math.tan(x)
-
-    
+ 
     print(round(x, 1), "\t", round(sin_x, 4), "\t", round(cos_x, 4), "\t", round(tan_x, 4))
 
     
