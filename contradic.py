7943dcd06903cd4b97bd7e92990e31c95902cf87
Cadet Adarsh <pal.adarsh10205@gmail.com>
2026-04-26 13:15:10 +0530

    Update contradic.py

--- contradic.py
+++ contradic.py
@@ -7,7 +7,7 @@ print("x\t sin(x)\t cos(x)\t tan(x)")
 while x <= 10:
     
     sin_x = math.sin(x)
-    cos_x = math.cos(x)
+    cos_x = math.cos(x)   
     tan_x = math.tan(x)
  
     print(round(x, 1), "\t", round(sin_x, 4), "\t", round(cos_x, 4), "\t", round(tan_x, 4))
